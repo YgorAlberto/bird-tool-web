9660f5a6bbf3096a85dcb83073a2fbb3070be0cc
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-05-15 23:37:16 -0300

    Update BIRD-SUB-TOOL.sh

--- BIRD-SUB-TOOL.sh
+++ BIRD-SUB-TOOL.sh
@@ -7,35 +7,57 @@ SUBDOMINIO="subdomains.txt"
 while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA assetfinder"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-assetfinder.sh $sublinha
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA subfinder"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-subfinder.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA sublist3r"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-sublist3r.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA dnsenum"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-dnsenum.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA dnsrecon"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-dnsrecon.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA fierce"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-fierce.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA amass"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-amass.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA nikto"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-nikto.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA wapiti"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-wapiti.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA nuclei"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-nuclei.sh $sublinha 2>/dev/null
     echo "ANALISANDO O DOMINIO $sublinha COM A FERRAMENTA hakrawler"
     echo " "
+done < "$SUBDOMINIO"
+while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
     sh tool-hakrawler.sh $sublinha 2>/dev/null
 done < "$SUBDOMINIO"
