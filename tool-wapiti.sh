df562491f4f12bfb6686961d0529b5763af565dd
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-05-21 14:11:04 -0300

    Update tool-wapiti.sh
    FOR em cada tool.sh
    Uma tool executa a outra
    arquivo domain trocado para target

--- tool-wapiti.sh
+++ tool-wapiti.sh
@@ -1,3 +1,1 @@
-mkdir -p OUT-WEB-BIRD/$1
-wapiti --scope domain -m all -d 10 -A Mozilla/5.0 -u "https://$1" >> OUT-WEB-BIRD/$1/$1-wapiti
-wapiti --scope domain -m all -d 10 -A Mozilla/5.0 -u "http://$1" >> OUT-WEB-BIRD/$1/$1-s-wapiti
+for target in $(cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && wapiti --scope domain -m all -d 10 -A Mozilla/5.0 -u "https://$target" >> OUT-WEB-BIRD/$target/$target-wapiti && wapiti --scope domain -m all -d 10 -A Mozilla/5.0 -u "http://$target" >> OUT-WEB-BIRD/$target/$target-s-wapiti && ./tool-nuclei.sh ;done
