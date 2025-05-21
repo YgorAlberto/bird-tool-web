6c9d798bccf1186886854a5629acb8cfbe81eb94
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-05-21 14:43:02 -0300

    Update tool-dnsenum.sh

--- tool-dnsenum.sh
+++ tool-dnsenum.sh
@@ -1,1 +1,1 @@
-for target in (cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && dnsenum -enum -w $target >> OUT-WEB-BIRD/$target/$target-dnsenum && ./tool-amass.sh ;done
+for target in $(cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && dnsenum -enum -w $target >> OUT-WEB-BIRD/$target/$target-dnsenum && ./tool-amass.sh ;done
