acd922910971eedc6962ba1089a9bf4d30d2de0a
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-05-21 14:10:48 -0300

    Update tool-amass.sh
    FOR em cada tool.sh
    Uma tool executa a outra
    arquivo domain trocado para target

--- tool-amass.sh
+++ tool-amass.sh
@@ -1,2 +1,1 @@
-mkdir -p OUT-WEB-BIRD/$1
-amass enum -d $1 >> OUT-WEB-BIRD/$1/$1-amass
+for target in $(cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && amass enum -d $target >> OUT-WEB-BIRD/$target/$target-amass && ./tool-dnsrecon.sh ;done
