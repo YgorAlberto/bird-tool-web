6e23eec29200084db3831b995ae768488b9c8cfb
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-05-21 14:42:49 -0300

    Update tool-subfinder.sh
    $

--- tool-subfinder.sh
+++ tool-subfinder.sh
@@ -1,1 +1,1 @@
-for target in (cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && subfinder --all -d $target >> OUT-WEB-BIRD/$target/$target-subfinder && ./tool-dnsenum.sh ;done
+for target in $(cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && subfinder --all -d $target >> OUT-WEB-BIRD/$target/$target-subfinder && ./tool-dnsenum.sh ;done
