61f759a7c096676e763bd4b23701b52602a99ac2
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-05-21 14:22:00 -0300

    Update BIRD-SUB-TOOL.sh
    Not needed anymore

--- BIRD-SUB-TOOL.sh
+++ BIRD-SUB-TOOL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo 'TAKING A LOOK ON SUBDOMAINS'
-echo " "
-SUBDOMINIO="subdomains.txt"
+#echo 'TAKING A LOOK ON SUBDOMAINS'
+#echo " "
+#SUBDOMINIO="subdomains.txt"
 
-./tool-assetfinder.sh
+#./tool-assetfinder.sh
 
 #for sub in $(cat subdomains.txt);do ./tool-assetfinder.sh $sub && ./tool-sublist3r.sh $sub && ./tool-subfinder.sh $sub && ./tool-dnsenum.sh $sub && ./tool-dnsrecon.sh $sub && ./tool-fierce.sh $sub && ./tool-amass.sh $sub && ./tool-nikto.sh $sub && ./tool-wapiti.sh $sub && ./tool-nuclei.sh $sub && ./tool-hakrawler.sh $sub ;done
 
