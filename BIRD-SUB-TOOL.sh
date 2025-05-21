27f070564659a9b001b2973a55f32bbbc69e2ac0
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-05-21 09:59:14 -0300

    Update BIRD-SUB-TOOL.sh

--- BIRD-SUB-TOOL.sh
+++ BIRD-SUB-TOOL.sh
@@ -4,35 +4,35 @@ echo 'TAKING A LOOK ON SUBDOMAINS'
 echo " "
 SUBDOMINIO="subdomains.txt"
 
-#!/bin/bash
+for sub in $(cat subdomains.txt);do ./tool-assetfinder.sh $sub && ./tool-sublist3r.sh $sub && ./tool-subfinder.sh $sub && ./tool-dnsenum.sh $sub && ./tool-dnsrecon.sh $sub && ./tool-fierce.sh $sub && ./tool-amass.sh $sub && ./tool-nikto.sh $sub && ./tool-wapiti.sh $sub && ./tool-nuclei.sh $sub && ./tool-hakrawler.sh $sub ;done
 
-while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
-    echo "======================================="
-    echo "[+] Iniciando análise para: $sublinha"
-    echo "======================================="
-
-    for tool in \
-        tool-amass.sh \
-        tool-assetfinder.sh \
-        tool-dnsenum.sh \
-        tool-dnsrecon.sh \
-        tool-fierce.sh \
-        tool-hakrawler.sh \
-        tool-nikto.sh \
-        tool-nuclei.sh \
-        tool-subfinder.sh \
-        tool-sublist3r.sh \
-        tool-wapiti.sh
-    do
-        echo "[+] Iniciando ${tool} para: $sublinha"
-        sh "$tool" "$sublinha" 2>/dev/null
-        echo "[✓] Finalizado ${tool} para: $sublinha"
-        echo ""
-    done
-
-    echo "======================================="
-    echo "[✓] Análise completa para: $sublinha"
-    echo "======================================="
-    echo ""
-done < "$SUBDOMINIO"
+#while IFS= read -r sublinha || [[ -n "$sublinha" ]]; do
+#    echo "======================================="
+#    echo "[+] Iniciando análise para: $sublinha"
+#    echo "======================================="
+#
+#    for tool in \
+#        tool-amass.sh \
+#        tool-assetfinder.sh \
+#        tool-dnsenum.sh \
+#        tool-dnsrecon.sh \
+#        tool-fierce.sh \
+#        tool-hakrawler.sh \
+#        tool-nikto.sh \
+#        tool-nuclei.sh \
+#        tool-subfinder.sh \
+#        tool-sublist3r.sh \
+#        tool-wapiti.sh
+#    do
+#        echo "[+] Iniciando ${tool} para: $sublinha"
+#        sh "$tool" "$sublinha" 2>/dev/null
+#        echo "[✓] Finalizado ${tool} para: $sublinha"
+#        echo ""
+#    done
+#
+#    echo "======================================="
+#    echo "[✓] Análise completa para: $sublinha"
+#    echo "======================================="
+#    echo ""
+#done < "$SUBDOMINIO"
 
