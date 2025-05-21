fc3f0df51334b135919dd9d1891604a7c6d3e3d6
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-05-21 10:22:08 -0300

    Update parsing-domains.sh

--- parsing-domains.sh
+++ parsing-domains.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd OUT-WEB-BIRD/
-grep -riah ".$1" | grep -v "grep" | grep -v "Trying" | grep -v "Scraping" | grep -v "\-\-\-" |grep -v "IN" | cut -d " " -f 1 | grep -via "\.arpa" |sort -u > ../subdomains-full.txt
+grep -riah ".$1" | grep -v "grep" | grep -v "Trying" | grep -v "Scraping" | grep -v "\-\-\-" |grep -v "IN" | cut -d " " -f 1 | grep -via "\.arpa" | grep ".$1" |sort -u > ../subdomains-full.txt
 
