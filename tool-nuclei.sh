027235a79e59ea04c643930050d80a73250442a1
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2025-10-15 13:03:48 -0300

    Delete tool-nuclei.sh

--- tool-nuclei.sh
+++
@@ -1,1 +0,0 @@
-for target in $(cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && nuclei -H "Mozilla/5.0" -u "http://"$target >> OUT-WEB-BIRD/$target/$target-nuclei && nuclei -H "Mozilla/5.0" -u "https://"$target >> OUT-WEB-BIRD/$target/$target-s-nuclei && ./tool-hakrawler.sh ;done
