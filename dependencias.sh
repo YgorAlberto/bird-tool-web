1f88329a1884a81ad64f1f0c911b1b4f21e6ee8f
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2026-01-13 19:51:06 -0300

    Add files via upload

---
+++ dependencias.sh
@@ -0,0 +1,7 @@
+sudo apt install amass assetfinder dnsenum dnsrecon fierce hakrawler nikto nuclei subfinder sublist3r 
+sudo apt install golang-go
+go install github.com/tomnomnom/waybackurls@latest
+sudo mv $(go env GOPATH)/bin/waybackurls /usr/local/bin/
+go install -v github.com/projectdiscovery/urlfinder/cmd/urlfinder@latest
+sudo mv $(go env GOPATH)/bin/urlfinder /usr/local/bin/
+
