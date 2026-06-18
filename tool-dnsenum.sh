46576425ca6810dce4b09d6e2c4b244538f33be4
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2026-06-18 13:24:22 -0300

    Update tool-dnsenum.sh
    Teste Timeout do DNSENUM na demora de pesquisa de alguns domains

--- tool-dnsenum.sh
+++ tool-dnsenum.sh
@@ -1,1 +1,1 @@
-for target in $(cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && dnsenum --enum $target >> OUT-WEB-BIRD/$target/$target-dnsenum;done
+for target in $(cat target.txt);do mkdir -p OUT-WEB-BIRD/$target && timeout 10m dnsenum --enum $target >> OUT-WEB-BIRD/$target/$target-dnsenum;done
